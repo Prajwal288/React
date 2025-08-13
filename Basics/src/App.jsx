e9d35fd851d053b7da0e10df6c42e030e8a194fd
Prajwal <koreprajwal288@gmail.com>
2025-08-13 21:41:04 +0530

    Add Basics and Custom react

---
+++ Basics/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const name = "Prajwal";
+  return (
+    <>
+      <h1>Vite React App {name}</h1>
+      <h1>Vite React App {2+2}</h1>
+
+    </>
+  )
+}
+
+export default App
--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
 # Start of react.js
+
+## Creating React App using vite
+    npm create vite@latest
+
+## create node mdolues in app
+   ```
+    npm i
+   ``` 
